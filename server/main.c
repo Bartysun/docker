17dc8ffd4c5999efcd4750e4d16d66dc39742ad7
Bartysun <olga.prague@ya.ru>
2025-06-25 14:23:35 +0300

    Add files via upload

---
+++ server/main.c
@@ -0,0 +1,20 @@
+/*
+ * hello.cpp
+ * Copyright (C) 2015 uralbash <root@uralbash.ru>
+ *
+ * Distributed under terms of the MIT license.
+ */
+// https://lectureswww.readthedocs.io/5.web.server/fcgi.html
+
+#include "fcgi_stdio.h"
+#include <stdlib.h>
+
+int main(void)
+{
+    while(FCGI_Accept() >= 0)
+    {
+        printf("Content-type: text/html\r\nStatus: 200 OK\r\n\r\nHello World!");
+    }
+
+    return 0;
+}
